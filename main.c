586026901248c62d0958421debd4ef1d6edb82f3
Carl Drinkwater <carl@29degrees.co.uk>
2009-07-24 13:24:20 +0100

    Add a copyright notice

--- main.c
+++ main.c
@@ -1,3 +1,11 @@
+//
+//  main.c
+//  carpod
+//
+//  Created by Carl Drinkwater on 24/07/2009.
+//  Copyright 2009 29degrees Limited. All rights reserved.
+//
+
 #include <stdio.h>
 #include "carpod.h"
 
